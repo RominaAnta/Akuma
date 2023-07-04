29c28bee6d65fdc252e6174dc796b35dac172835
Romina Anta <rominaa.87@gmail.com>
2023-07-03 21:56:30 -0300

    integracion backend

---
+++ static/js/form2.js
@@ -0,0 +1,10 @@
+const form = document.querySelector('#signup-form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); 
+  
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const comment = form.elements.comment.value;
+  
+  form.reset();
+});
